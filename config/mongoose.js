879e3433da50ee132dafdf32c80db08c9346ca5c
M Haidar Hanif <git@mhaidarhanif.com>
2020-07-25 03:09:06 +0700

    Configure mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/test'
+
+mongoose.set('useCreateIndex', true)
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+module.exports = mongoose
