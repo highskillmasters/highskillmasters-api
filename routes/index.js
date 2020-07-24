7d93e8c9a61ddb4b6eab69379d7945456a238777
M Haidar Hanif <git@mhaidarhanif.com>
2020-07-25 03:09:19 +0700

    Create and use mongoose model

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+const Member = require('./members/model')
+
 router.get('/', (req, res) => {
   res.send({
     title: 'High Skill Masters API',
   })
 })
 
-router.post('/subscribe', (req, res) => {
-  res.send({
-    message: 'Email is successfully subscribed.',
+router.post('/subscribe', async (req, res) => {
+  const newMember = {
     email: req.body.email,
+  }
+
+  await Member.create(newMember)
+
+  res.send({
+    message: 'Email is successfully subscribed',
+    email: newMember.email,
   })
 })
 
---
+++ routes/members/model.js
@@ -0,0 +1,25 @@
+const mongoose = require('../../config/mongoose')
+const AutoIncrement = require('mongoose-sequence')(mongoose)
+const Schema = mongoose.Schema
+
+const MemberSchema = Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      min: [2, 'Email is too short'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+MemberSchema.plugin(AutoIncrement, {
+  id: 'memberss_counter',
+  inc_field: 'id',
+})
+
+const Member = mongoose.model('Member', MemberSchema)
+
+module.exports = Member
