e63ff8990665459e3130271fa61b1e542b1f80cf
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-07-25 09:20:38 +0700

    Connect to database in index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router()
 
 const { index, createMember } = require('./controller')
 
+// Connect to database
+require('../config/mongoose')
+
 router.get('/', index)
 router.post('/members', createMember)
 
