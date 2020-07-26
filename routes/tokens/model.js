cf8b5636e4a106b202a3464cd20efdd62ed8a362
M Haidar Hanif <git@mhaidarhanif.com>
2020-07-27 01:29:47 +0700

    Improve token model

--- routes/tokens/model.js
+++ routes/tokens/model.js
@@ -1,23 +1,27 @@
 const mongoose = require('../../config/mongoose')
 const Schema = mongoose.Schema
 
-const TokenSchema = Schema({
-  memberId: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: 'Member',
+const TokenSchema = Schema(
+  {
+    memberId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Member',
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+    isUsed: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  token: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now,
-    expires: 600, // Expire in 10 minutes
-  },
-})
+  {
+    timestamps: true,
+  }
+)
 
 const Token = mongoose.model('Token', TokenSchema)
 
